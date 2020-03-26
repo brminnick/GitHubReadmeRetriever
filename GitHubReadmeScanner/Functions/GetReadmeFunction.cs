65e90bf67fe36bb08e6ade76ee047e89e510f2c2
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2020-03-26 12:20:38 -0700

    Create GetReadmeFunction.cs

---
+++ GitHubReadmeScanner/Functions/GetReadmeFunction.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
+
+namespace GitHubReadmeScanner
+{
+    class GetReadmeFunction
+    {
+        readonly GitHubUserContentApiService _gitHubUserContentApiService;
+
+        public GetReadmeFunction(GitHubUserContentApiService gitHubUserContentApiService) => _gitHubUserContentApiService = gitHubUserContentApiService;
+
+        [FunctionName(nameof(GetReadmeFunction))]
+        public Task<ReadmeModel> Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "GetReadme/{ownerName}/{repositoryName}")] HttpRequest req, string ownerName, string repositoryName, ILogger log)
+        {
+            log.LogInformation($"Retrieving Readme for {repositoryName} by {ownerName}");
+
+            return _gitHubUserContentApiService.GetReadme(ownerName, repositoryName);
+        }
+    }
+}
